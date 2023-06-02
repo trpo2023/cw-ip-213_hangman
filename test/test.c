e1f2278a8000793afdb135763eaf016f2768daf1
Egor Dmitriev <edmitriev377@gmail.com>
2023-06-02 16:26:16 +0700

    modified test.c

--- test/test.c
+++ test/test.c
@@ -25,3 +25,11 @@ CTEST(check_type_of_category, correct_category_3)
     const char *result = Word(category);
     ASSERT_STR(expected, result);
 }
+
+CTEST(check_type_of_category, incorrect_category)
+{
+    int category = 4;
+    const char *excpected = NULL;
+    const char *result = Word(category);
+    ASSERT_STR(excpected, result);
+}
